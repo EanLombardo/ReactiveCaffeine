5a0c355000adc83654fc28592f25540ba4cdc0d2
eanlom <ean.lombardo@asynchrony.com>
2016-02-20 23:07:46 -0600

    Added Javadoc for FluentObservable

--- src/main/java/com/rxc/FluentObservable.java
+++ src/main/java/com/rxc/FluentObservable.java
@@ -1,10 +1,26 @@
 package com.rxc;
 
+/**
+ * A simple system to create an observable that performs a series of events
+ */
 public class FluentObservable {
+
+    /**
+     * Starts building a FluentObservable that will notify it's subscribers once it starts emitting events
+     * @param clazz The class for the type of Observable
+     * @param <T> The class for the type of Observable
+     * @return A FluentObservableBuilder to construct the rest of the Observable with
+     */
     public static <T> FluentObservableBuilder<T> thatStarts(Class<T> clazz) {
         return new FluentObservableBuilder<T>().thatStarts();
     }
 
+    /**
+     * Starts building a FluentObservable that will not notify it's subscribers once it starts emitting events
+     * @param clazz The class for the type of Observable
+     * @param <T> The class for the type of Observable
+     * @return A FluentObservableBuilder to construct the rest of the Observable with
+     */
     public static <T> FluentObservableBuilder<T> thatNeverStarts(Class<T> clazz){
         return new FluentObservableBuilder<T>();
     }
--- src/main/java/com/rxc/FluentObservableBuilder.java
+++ src/main/java/com/rxc/FluentObservableBuilder.java
@@ -8,6 +8,10 @@ import rx.functions.Action1;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A builder to build FluentObservables
+ * @param <T> The class for the Observable
+ */
 public class FluentObservableBuilder<T> {
     private final List<Event<T>> events = new LinkedList<Event<T>>();
 
@@ -20,31 +24,59 @@ public class FluentObservableBuilder<T> {
         return this;
     }
 
+    /**
+     * Adds a new event to the observable that simply emits the item given
+     * @param item The item to emit
+     * @return The builder to build the rest of the observable with
+     */
     public FluentObservableBuilder<T> thenEmits(final T item){
         events.add(new NextEvent<T>(item));
         return this;
     }
 
+    /**
+     * Adds a new event to the observable that errors using the given error
+     * @param error The error
+     * @return The builder to build the rest of the observable with
+     */
     public FluentObservableBuilder<T> thenErrors(final Throwable error){
         events.add(new ErrorEvent<T>(error));
         return this;
     }
 
+    /**
+     * Adds a new event to the observable that will delay all further event's by the specified amount of time
+     * @param mills How long to delay in milliseconds
+     * @return The builder to build the rest of the observable with
+     */
     public FluentObservableBuilder<T> thenWaits(final long mills){
         events.add(new SleepEvent<T>(mills));
         return this;
     }
 
+    /**
+     * Adds a new event to the observable that performs the specified action on the subscription
+     * @param action The action to be performed
+     * @return The builder to build the rest of the observable with
+     */
     public FluentObservableBuilder<T> then(final Action1<Subscriber<? super T>> action){
         events.add(new ActionEvent<T>(action));
         return this;
     }
 
+    /**
+     * Builds the final FluentObservable, the last action of the Observable will be to notify it's subscribers that it completed
+     * @return The Observable that will emit the defined events when subscribed to
+     */
     public Observable<T> thenCompletes(){
         events.add(new CompletionEvent<T>());
         return build();
     }
 
+    /**
+     * Builds the final FluentObservable, the observable will stop performing events, but it will never inform it's subscribers of it's completion
+     * @return The Observable that will emit the defined events when subscribed to
+     */
     public Observable<T> thenNeverCompletes(){
         return build();
     }
